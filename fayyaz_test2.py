94f15621db850c7106d7325f1a3c2f96bafa3c70
venom-GH <77876795+venom-GH@users.noreply.github.com>
2021-01-27 21:09:37 +0400

    Create fayyaz_test2.py
    range function second part

---
+++ fayyaz_test2.py
@@ -0,0 +1,11 @@
+for j in range (5):
+    print ("*")
+    print ("./ \n   hellow")
+    print("yes")
+    print(j)
+
+
+
+a=['sarika','Aditya','mohd']
+for i in a:
+    print ("fayyaz teaches "+i, end=" ")
