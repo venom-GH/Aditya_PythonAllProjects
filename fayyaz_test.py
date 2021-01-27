c324c0233c9071a30bec4cf23eaca9526204c522
venom-GH <77876795+venom-GH@users.noreply.github.com>
2021-01-27 13:54:22 +0400

    Create fayyaz_test.py
    in range function

---
+++ fayyaz_test.py
@@ -0,0 +1,11 @@
+for i in range (5):
+    print (i)
+    for j in range (3+i):
+        if i==2:
+            break
+        print (" "+str(j))
+    print("hellow im cool")
+    if i==2:
+        break
+
+input("Press ENTER to continue..")
