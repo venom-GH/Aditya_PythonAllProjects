a42b1d8e39f5463d3a6e2f388089946bb10d6bc0
venom-GH <77876795+venom-GH@users.noreply.github.com>
2021-01-27 21:10:14 +0400

    Create fayyaz_test4.py
    while loop

---
+++ fayyaz_test4.py
@@ -0,0 +1,11 @@
+counter=0
+
+while counter < 5:
+    print ("Hellow World", counter)
+    counter = counter + 1
+
+print()
+print()
+
+for counter1 in range(3):
+    print ("Hellow Wolrd", counter1)
